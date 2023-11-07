a624af1874391f0810e42e5705732fac0dace17c
GSilva97 <gleydsonjs97@outlook.com>
2023-11-07 18:47:28 -0300

    criando DTO de caixa de atendimento

---
+++ src/main/java/br/com/carrancas/start/minhavez/dto/CaixaAtendimentoRequestDTO.java
@@ -0,0 +1,21 @@
+package br.com.carrancas.start.minhavez.dto;
+
+import br.com.carrancas.start.minhavez.entities.CaixaAtendimento;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CaixaAtendimentoRequestDTO {
+    private String nomeAtendente;
+    private boolean status;
+
+    public static CaixaAtendimento toDTO(CaixaAtendimentoRequestDTO caixaAtendimentoDTO) {
+        return CaixaAtendimento.builder()
+                .nomeAtendente(caixaAtendimentoDTO.getNomeAtendente())
+                .status(caixaAtendimentoDTO.isStatus())
+                .build();
+    }
+}
