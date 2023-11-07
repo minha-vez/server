32c5925bf829efa190a65bf5b75eb739e6a77054
FHenric <ffilipehhenrique@gmail.com>
2023-11-07 18:24:15 -0300

    Configurando DTO da entidade Pessoa e adicionando telefone na entidade Pessoa

---
+++ src/main/java/br/com/carrancas/start/minhavez/dto/PessoaRequestDTO.java
@@ -0,0 +1,31 @@
+package br.com.carrancas.start.minhavez.dto;
+
+import br.com.carrancas.start.minhavez.entities.Pessoa;
+import br.com.carrancas.start.minhavez.eums.Genero;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PessoaRequestDTO {
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private Date nascimento;
+    private Genero genero;
+
+    public static Pessoa toEntity(PessoaRequestDTO PessoaRequestDTO) {
+        return Pessoa.builder()
+                .nome(PessoaRequestDTO.getNome())
+                .cpf(PessoaRequestDTO.getCpf())
+                .telefone(PessoaRequestDTO.getTelefone())
+                .nascimento(PessoaRequestDTO.getNascimento())
+                .genero(PessoaRequestDTO.getGenero())
+                .build();
+    }
+}
