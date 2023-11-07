bd7620a7fcc6f64dedd05c34b2035e2474d9403b
Marcondes Júnior <jrmoriaproducoes1996@gmail.com>
2023-11-07 18:17:23 -0300

    Criando Dto de empresa

---
+++ src/main/java/br/com/carrancas/start/minhavez/dto/EmpresaRequestDto.java
@@ -0,0 +1,44 @@
+package br.com.carrancas.start.minhavez.dto;
+
+import br.com.carrancas.start.minhavez.entities.Empresa;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmpresaRequestDto {
+
+    private String nome;
+    private String cnpj;
+    private String endereco;
+    private String telefone;
+    private String telefoneParaEmergencia;
+    private String email;
+    private String website;
+    private LocalDateTime horaInicio;
+    private LocalDateTime horaFinal;
+    private Boolean status;
+
+
+    public static Empresa toEntity(EmpresaRequestDto empresaRequestDto){
+        return Empresa.builder()
+                .cnpj(empresaRequestDto.getCnpj())
+                .nome(empresaRequestDto.getNome())
+                .endereco(empresaRequestDto.getEndereco())
+                .telefone(empresaRequestDto.getTelefone())
+                .website(empresaRequestDto.getWebsite())
+                .telefoneParaEmergencia(empresaRequestDto.getTelefoneParaEmergencia())
+                .email(empresaRequestDto.getEmail())
+                .website(empresaRequestDto.getWebsite())
+                .horaInicio(empresaRequestDto.getHoraInicio())
+                .horaFinal(empresaRequestDto.getHoraFinal())
+                .status(Boolean.TRUE)
+                .build();
+    }
+
+
+}
