8c3c625167425091d9f68d438a0450b9c8b748d5
Marcondes Júnior <jrmoriaproducoes1996@gmail.com>
2023-11-09 15:12:50 -0300

    Criando o controller de empresa

--- src/main/java/br/com/carrancas/start/minhavez/services/EmpresaService.java
+++ src/main/java/br/com/carrancas/start/minhavez/services/EmpresaService.java
@@ -1,4 +1,4 @@
-package br.com.carrancas.start.minhavez.service;
+package br.com.carrancas.start.minhavez.services;
 
 import br.com.carrancas.start.minhavez.dto.request.EmpresaRequestDto;
 import br.com.carrancas.start.minhavez.dto.response.EmpresaResponseDTO;
@@ -7,15 +7,27 @@ import br.com.carrancas.start.minhavez.repositories.EmpresaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class EmpresaService {
 
     private final EmpresaRepository empresaRepository;
 
-    public EmpresaResponseDTO criar(EmpresaRequestDto empresaRequestDto){
+    //Create Read Update Delete (CRUD)
+
+    public EmpresaResponseDTO criar (EmpresaRequestDto empresaRequestDto){
         Empresa empresa = EmpresaRequestDto.toEntity(empresaRequestDto);
         empresaRepository.save(empresa);
         return EmpresaResponseDTO.toDto(empresa);
+    }
+
+    public List<EmpresaResponseDTO> listarEmpresas(){
+        List<Empresa> empresas = empresaRepository.findAll();
+        return empresas.stream().map(empresa -> EmpresaResponseDTO.toDto(empresa)).collect(Collectors.toList());
+
+
     }
 }
