f186318688e912855d740163f75212762646c402
Marcondes Júnior <jrmoriaproducoes1996@gmail.com>
2023-11-09 15:15:19 -0300

    apagando pasta service, duplicada

--- src/main/java/br/com/carrancas/start/minhavez/services/CaracteristicaService.java
+++ src/main/java/br/com/carrancas/start/minhavez/services/CaracteristicaService.java
@@ -1,4 +1,4 @@
-package br.com.carrancas.start.minhavez.service;
+package br.com.carrancas.start.minhavez.services;
 
 import br.com.carrancas.start.minhavez.dto.request.CaracteristicaRequestDto;
 import br.com.carrancas.start.minhavez.dto.response.CaracteristicaResponseDTO;
