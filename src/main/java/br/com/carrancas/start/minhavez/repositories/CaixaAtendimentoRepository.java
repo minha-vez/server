00ea823b92601a86dee5037de1d3d0d9d9394ff4
GSilva97 <gleydsonjs97@outlook.com>
2023-11-11 10:09:02 -0300

    criando o repository e o service do caixa atendimento

---
+++ src/main/java/br/com/carrancas/start/minhavez/repositories/CaixaAtendimentoRepository.java
@@ -0,0 +1,7 @@
+package br.com.carrancas.start.minhavez.repositories;
+
+import br.com.carrancas.start.minhavez.entities.CaixaAtendimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CaixaAtendimentoRepository extends JpaRepository<CaixaAtendimento, Integer> {
+}
