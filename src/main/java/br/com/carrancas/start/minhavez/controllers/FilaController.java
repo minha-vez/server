06ccc90d873de356519300956c503808bbb29a1e
GSilva97 <gleydsonjs97@outlook.com>
2023-11-21 17:30:02 -0300

    criando os StatusHttp dos controllers de: especialidade, convenio e fila

--- src/main/java/br/com/carrancas/start/minhavez/controllers/FilaController.java
+++ src/main/java/br/com/carrancas/start/minhavez/controllers/FilaController.java
@@ -4,6 +4,7 @@ import br.com.carrancas.start.minhavez.dto.request.FilaRequestDto;
 import br.com.carrancas.start.minhavez.dto.response.FilaResponseDto;
 import br.com.carrancas.start.minhavez.services.FilaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -13,8 +14,9 @@ public class FilaController {
 
     private final FilaService filaService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/empresa/{empresaId}")
-    public FilaResponseDto criar(@PathVariable int empresaId,@RequestBody FilaRequestDto filaRequestDto){
+    public FilaResponseDto criar(@PathVariable int empresaId, @RequestBody FilaRequestDto filaRequestDto) {
         return filaService.criar(empresaId, filaRequestDto);
     }
 }
