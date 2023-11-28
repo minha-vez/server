cc354a67a223e0745262d76d185261c74ee3cf1f
FHenric <ffilipehhenrique@gmail.com>
2023-11-28 00:54:08 -0300

    Adicionamos a annotation @CrossOrigin nas cntrollers e acrescentamos o funcionario na lsita de permissão de acesso ao metodo criar (ControllerCliente)

--- src/main/java/br/com/carrancas/start/minhavez/controllers/ConvenioController.java
+++ src/main/java/br/com/carrancas/start/minhavez/controllers/ConvenioController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/convenios")
 @RequiredArgsConstructor
