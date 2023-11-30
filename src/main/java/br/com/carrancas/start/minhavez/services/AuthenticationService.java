4eef27570252d6c0f434ecef486af28fec9935c6
Marcondes Júnior <jrmoriaproducoes1996@gmail.com>
2023-11-30 02:30:55 -0300

    Criando metodo para decodificar token

--- src/main/java/br/com/carrancas/start/minhavez/services/AuthenticationService.java
+++ src/main/java/br/com/carrancas/start/minhavez/services/AuthenticationService.java
@@ -4,12 +4,20 @@ import br.com.carrancas.start.minhavez.config.JwtService;
 import br.com.carrancas.start.minhavez.dto.request.AuthenticationRequest;
 import br.com.carrancas.start.minhavez.dto.response.AuthenticationResponse;
 import br.com.carrancas.start.minhavez.repositories.UsuarioRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +25,8 @@ public class AuthenticationService {
     private final UserDetailsService userDetailsService;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
-
-    private final UsuarioRepository usuarioRepository;
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         authenticationManager.authenticate(
@@ -36,6 +44,25 @@ public class AuthenticationService {
                 .accessToken(jwtToken)
                 .email(userDetails.getUsername())
                 .build();
+    }
+
+    public ResponseEntity<?> decodeToken(String token) {
+        try {
+            Claims body = Jwts
+                    .parserBuilder()
+                    .setSigningKey(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return ResponseEntity.ok(body); // Retorna o payload decodificado como JSON
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Erro ao decodificar o token: " + e.getMessage());
+        }
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
 }
